db623017e1f6a3871887760aa172cce7a4e8aad8
oscar1 <wizzofozz@cuckoosnest.max3sat.nl>
2019-08-31 10:28:58 +0200

    Initial

---
+++ DES_CBC_PaddingOracle_demo/CBC_DES_Oracle/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.plainBox = new System.Windows.Forms.TextBox();
+            this.cypherBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.IVBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // plainBox
+            // 
+            this.plainBox.Location = new System.Drawing.Point(126, 24);
+            this.plainBox.Name = "plainBox";
+            this.plainBox.Size = new System.Drawing.Size(227, 20);
+            this.plainBox.TabIndex = 1;
+            this.plainBox.Text = "type text and click encrypt, after that click go";
+            // 
+            // cypherBox
+            // 
+            this.cypherBox.Location = new System.Drawing.Point(126, 122);
+            this.cypherBox.Name = "cypherBox";
+            this.cypherBox.Size = new System.Drawing.Size(227, 20);
+            this.cypherBox.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(126, 51);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(227, 20);
+            this.keyBox.TabIndex = 4;
+            this.keyBox.Text = "12345678";
+            // 
+            // IVBox
+            // 
+            this.IVBox.Location = new System.Drawing.Point(126, 87);
+            this.IVBox.Name = "IVBox";
+            this.IVBox.Size = new System.Drawing.Size(227, 20);
+            this.IVBox.TabIndex = 5;
+            this.IVBox.Text = "7B216A634951170F";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Plaintext";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "IV";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Cyphertext";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Length cipher:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(278, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(31, 229);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(322, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Debug";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 304);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IVBox);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cypherBox);
+            this.Controls.Add(this.plainBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Padding oracle Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox plainBox;
+        private System.Windows.Forms.TextBox cypherBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.TextBox IVBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ DES_CBC_PaddingOracle_demo/CBC_DES_Oracle/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] hextobytes(string hexstr)
+        {
+            var arr = new byte[hexstr.Length / 2];
+            for (var i = 0; i < arr.Length; i++)
+                arr[i] = (byte)Convert.ToInt32(hexstr.Substring(i * 2, 2), 16);
+            return arr;
+        }
+
+        private DESCryptoServiceProvider getProvider()
+        {
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            //provider.BlockSize = 8;
+            provider.Key = System.Text.Encoding.ASCII.GetBytes(keyBox.Text);
+            provider.IV = hextobytes(IVBox.Text);
+            provider.Mode = CipherMode.CBC;
+            return provider;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DESCryptoServiceProvider provider = getProvider();
+            
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, provider.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainBox.Text);
+                    }
+                    //cypherBox.Text = Convert.ToBase64String( msEncrypt.ToArray());
+                    cypherBox.Text = BitConverter.ToString(msEncrypt.ToArray()).Replace("-", "");
+                    label5.Text = "Length cipher: " + cypherBox.Text.Length/2;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        
+            DESCryptoServiceProvider provider = getProvider();
+
+           // using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cypherBox.Text)))
+            using (MemoryStream msDecrypt = new MemoryStream(hextobytes(cypherBox.Text)))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, provider.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        // Read the decrypted bytes from the  decrypting stream 
+                        // and place them in a string.
+                        try
+                        {
+                            plainBox.Text = srDecrypt.ReadToEnd();
+                        } catch (Exception exc) // fix thus handler? dont rememebr
+                        {
+                            MessageBox.Show(exc.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private string decrypt(byte[] iv, byte[]ciphertext)
+        {
+            string retval;
+            DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+            //provider.BlockSize = 8;
+            provider.Key = System.Text.Encoding.ASCII.GetBytes(keyBox.Text);
+            provider.IV = iv;
+            provider.Mode = CipherMode.CBC;
+            using (MemoryStream msDecrypt = new MemoryStream(ciphertext))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, provider.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            // Read the decrypted bytes from the  decrypting stream 
+                            // and place them in a string.
+
+                           retval = srDecrypt.ReadToEnd();
+                            
+                        }
+                    
+                }
+            }
+            return retval;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 
+            string h = "0000000000000000";
+            byte[] origiv = hextobytes(IVBox.Text);
+            string plaintext = "";
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = cypherBox.Text.Length / 2;
+            progressBar1.Step = 1;
+
+            for (int n = 0; n < cypherBox.Text.Length / 16; n++) // loop through 8 byte blocks
+            {
+                string block = cypherBox.Text.Substring(n*16, 16);
+                byte[] intermediates = hextobytes(h);
+                byte[] iv = hextobytes(h);
+                string plainblock = "";
+
+                for (int k = 7; k >= 0; k--) // loop through each byte
+                {
+                    byte padval = (byte)(8 - k);
+
+                    for (int i = 0; i < 256; i++) // loop through 256 vals until no exception thrown
+                    {
+                        iv[k] = (byte)i;
+                        try
+                        {
+                            decrypt(iv, hextobytes(block)); // throws if padding incorrect, here we exploit that knowlegde
+                            // 200 OK
+                            label5.Text = "No error at " + i;
+                            intermediates[k] = (byte)(i ^ padval);
+                            
+                            plainblock = ((char)(intermediates[k] ^ origiv[k])).ToString() + plainblock;
+                            textBox1.Text = plainblock;
+ 
+                            //byte nextpad = (byte)(interm ^ (8-k+1));
+
+                            for (int j = 0; j < padval; j++)
+                            {
+                                iv[7 - j] = (byte)((padval + 1) ^ intermediates[7 - j]);
+                            }
+                            progressBar1.Value += 1;
+                            break;
+                        }
+                        catch (CryptographicException exc)
+                        {
+                            // 500 Internal Server Error
+                            label5.Text = "Exception" + exc.Message + " at " + i;
+
+                        }
+                        Application.DoEvents();
+                    }
+                }
+                origiv = hextobytes(block);
+                plaintext += plainblock;
+            }
+            MessageBox.Show("Plaintext: " + plaintext);
+        }
+    }
+}
